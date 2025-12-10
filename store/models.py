2c8a3bb5130f0a64b24189151071c795653f9b96
Shokhrukh Nigmatillaev <shoxrux020902@gmail.com>
2025-12-11 00:57:30 +0500

    Add many-to-many relationship

--- store/models.py
+++ store/models.py
@@ -1,17 +1,24 @@
 from django.db import models
 
+
 # Create your models here.
 class Collection(models.Model):
     title = models.CharField(max_length=255)
+
 
 class Product(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
     inventory = models.IntegerField()
-    last_update = models.DateTimeField(auto_now=True) # auto_now every time we update the product object this property sets time to that date/time
-#                                                     auto_now_add - with this only time when we create Product will be stored
+    last_update = models.DateTimeField(
+        auto_now=True)  # auto_now every time we update the product object this property sets time to that date/time
+    #                                                     auto_now_add - with this only time when we create Product will be stored
     collection = models.ForeignKey('Collection', on_delete=models.PROTECT)
+    promotions = models.ManyToManyField(
+        'Promotion')  # related_names='products' we can add this if we want to make custom name for field
+
+
 class Customer(models.Model):
     MEMBERSHIP_BRONZE = 'B'
     MEMBERSHIP_SILVER = 'S'
@@ -28,12 +35,15 @@ class Customer(models.Model):
     phone = models.CharField(max_length=255)
     birth_date = models.DateField(null=True)
     membership = models.CharField(max_length=1, choices=MEMBERSHIP_CHOICES, default=MEMBERSHIP_BRONZE)
+
+
 class Item(models.Model):
     title = models.CharField(max_length=255)
     order = models.ForeignKey('Order', on_delete=models.PROTECT)
     product = models.ForeignKey(Product, on_delete=models.PROTECT)
     amount = models.PositiveSmallIntegerField()
     unit_price = models.DecimalField(max_digits=6, decimal_places=2)
+
 
 class Order(models.Model):
     placed_at = models.DateTimeField(auto_now_add=True)
@@ -50,13 +60,16 @@ class Order(models.Model):
     payment_status = models.CharField(max_length=1, choices=PAYMENT_STATUS_CHOICES, default=PENDING_STATUS)
     customer = models.ForeignKey('Customer', on_delete=models.PROTECT)
 
+
 class Cart(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
+
 
 class CartItem(models.Model):
     cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.PositiveSmallIntegerField()
+
 
 # One-to-One database relationship
 class Address(models.Model):
@@ -64,8 +77,13 @@ class Address(models.Model):
     city = models.CharField(max_length=255)
     customer = models.OneToOneField(Customer, on_delete=models.CASCADE, primary_key=True)
 
+
 # One-to-Many database relationship
 # class Address(models.Model):
 #     street = models.CharField(max_length=255)
 #     city = models.CharField(max_length=255)
 #     customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+
+class Promotion(models.Model):
+    description = models.CharField(max_length=255)
+    discount = models.FloatField()
