31ed27f87c94ceb6525217d07c74cbf52a525309
Shokhrukh Nigmatillaev <shoxrux020902@gmail.com>
2025-12-11 01:43:32 +0500

    Add generic relationships example

--- likes/models.py
+++ likes/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Likes(models.Model):
+    label = models.CharField(max_length=255)
+
+class LikedItem(models.Model):
+    like = models.ForeignKey(Likes, on_delete=models.CASCADE)
